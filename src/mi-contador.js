c3a280a33681d9d350b4df80897e4359f430e0bc
katherinebl <kath.gbl@gmail.com>
2020-06-19 13:56:00 +0200

    Included feedback component into the counter component and defined a getter to DRY code

--- src/mi-contador.js
+++ src/mi-contador.js
@@ -28,17 +28,33 @@ export class MiContador extends LitElement {
 
         <button @click="${this.incrementar}">+1</button>
         <button @click="${this.decrementar}">-1</button>
+
+        <feedback-element id="feedback"></feedback-element>
       </div>
     `;
   }
 
   //Métodos
+
+  // Getter
+  get feedback() {
+    return this.shadowRoot.getElementById("feedback");
+  }
+
   incrementar() {
     this.counter++;
+
+    if (this.counter == 3) {
+      this.feedback.open("You've reached the top!");
+    }
   }
 
   decrementar() {
     this.counter--;
+
+    if (this.counter == 0) {
+      this.feedback.open("You're out of clicks!");
+    }
   }
 }
 customElements.define("mi-contador", MiContador);
